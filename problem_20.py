797dfeff2ecc34849ba9fe5cfa2f1c6eb6c242cf
Jiaming <zhoujiaming12345@gmail.com>
2020-03-12 21:23:27 +0800

    括号匹配，使用了栈

---
+++ problem_20.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+# Created by: Jiaming
+# Created at: 2020-03-12
+
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+#
+# 有效字符串需满足：
+#
+# 左括号必须用相同类型的右括号闭合。
+# 左括号必须以正确的顺序闭合。
+# 注意空字符串可被认为是有效字符串。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/valid-parentheses
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_stack = list()
+        for c in s:
+            if c in ('(', '[', '{'):
+                my_stack.append(c)
+            else:
+                if my_stack and my_stack[-1] == {']': '[', '}': '{', ')': '('}.get(c):
+                    my_stack.pop()
+                else:
+                    return False
+        return len(my_stack) == 0
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isValid("()"))
+    print(s.isValid("()[]{}"))
+    print(s.isValid("(]"))
+    print(s.isValid("([)]"))
+    print(s.isValid("{[]}"))
+    print(s.isValid("["))
